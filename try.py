e958a2704aff9061fcffa248f8cb2be935d3f50a
E.Evmenova <e.evmenova.o@gmail.com>
2020-08-28 14:15:56 +0300

    Update try.py

--- try.py
+++ try.py
@@ -10,13 +10,14 @@ attributive_graph = nx.erdos_renyi_graph(500, 0.05)
 N_edges = len(attributive_graph.edges())
 print('Number of edges in attributive ER-graph: ', N_edges)
 
+alpha = 1
+partition = louvain_modified_algorithm.best_partition(structural_graph, attributive_graph, alpha=alpha)
 Q1 = louvain_modified_algorithm.modularity(
-    louvain_modified_algorithm.best_partition(structural_graph, attributive_graph, alpha=1),
+    partition,
     structural_graph,
     attributive_graph,
-    alpha=1)
+    alpha=alpha)
 print(Q1)
 
-Q2 = community_louvain.modularity(community_louvain.best_partition(structural_graph),
-                                  structural_graph)
+Q2 = community_louvain.modularity(partition, structural_graph)
 print(Q2)
